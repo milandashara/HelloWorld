6d367f465d59bd1d2bddd49ddf02ec24d967d1a0
student <student@CR33S28I.local>
2014-04-13 01:14:22 +0800

    Milan

---
+++ Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by student on 4/12/14.
+//  Copyright (c) 2014 sg.edu.nus.iss.wlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello World/Hello World/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by student on 4/12/14.
+//  Copyright (c) 2014 sg.edu.nus.iss.wlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
